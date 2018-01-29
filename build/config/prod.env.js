81e433f343679e28f5512e46ac570347c437644f
shanghaitao <359310444@qq.com>
2018-01-29 17:25:37 +0800

    up no more qa

--- build/config/prod.env.js
+++ build/config/prod.env.js
@@ -2,6 +2,6 @@ const getConfig = require("./getConfig");
 
 module.exports = {
     NODE_ENV: '"production"',
-    APP_ENV: `"${process.env.APP_ENV || "prod"}"`,
+    APP_ENV: '"prod"',
     ENV_CONFIG: JSON.stringify(getConfig())
 };
